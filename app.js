28ac17b6337a3fe742cef9f6d564694e3d759d03
Samuel Raducan <samuel.thembp@gmail.com>
2022-04-16 12:58:01 +0100

    Setup project

---
+++ app.js
@@ -0,0 +1,14 @@
+
+/**
+ * http://vidly.com/api/generes
+ * 
+ * http://localhost:3000/api/generes
+ * 
+ * TODO: 
+ * Requests: 
+ *  - get the genres
+ *  - post (create a new one)
+ *  - update 
+ *  - delete 
+ */
+

6f20f05e5efa0ddf1331ea62ab33e77eaf9538c6
Samuel Raducan <samuel.thembp@gmail.com>
2022-04-16 21:34:46 +0100

    Create put method to update a genre and add input validation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Joi = require('joi');
 const express = require('express');
 const { join } = require('path');
+const { validate } = require('isemail');
 const app = express();
 
 // Middleware for json data
@@ -19,22 +20,27 @@ app.use(express.json());
  */
 
 const genres = [
-	{ id: 1, genre: 'Action' },
-	{ id: 2, genre: 'Thriller' },
-	{ id: 3, genre: 'Romance' },
-	{ id: 4, genre: 'Drama' },
-	{ id: 5, genre: 'Science Fiction' },
+	{ id: 1, name: 'Action' },
+	{ id: 2, name: 'Thriller' },
+	{ id: 3, name: 'Romance' },
+	{ id: 4, name: 'Drama' },
+	{ id: 5, name: 'Science Fiction' },
 ];
 
-// Get request for all the genres
+// Get -> retrieve all the genres
 app.get('/api/genres', (req, res) => {
 	res.send(genres);
 });
 
+// Post -> create a new genre
 app.post('/api/genres', (req, res) => {
+	// Validation
+	const result = validateGenre(req.body);
+	if (result) return res.status(400).send(result.error.details[0].message);
+
 	const genre = {
 		id: genres.length + 1,
-		genre: req.body.genre,
+		name: req.body.name,
 	};
 
 	genres.push(genre);
@@ -42,6 +48,29 @@ app.post('/api/genres', (req, res) => {
 	// Send the genre in the body of the response
 	res.send(genre);
 });
+
+// Put -> Update a genre
+app.put('/api/genres/:id', (req, res) => {
+	const genre = genres.find((genre) => genre.id === parseInt(req.params.id));
+	if (!genre)
+		return res.status(404).send('The genre with the given ID was not found.');
+
+	// Error when the updated data does not match the validation
+	const result = validateGenre(genre);
+	if (result) return res.status(400).send(result.error);
+
+	genre.name = req.body.name;
+	res.send(genre);
+});
+
+// Genre validation
+function validateGenre(genre) {
+	const schema = {
+		name: Joi.string().min(3).required(),
+	};
+
+	return Joi.validate(genre, schema, (err, value) => {});
+}
 
 // Dynamic PORT
 const port = process.env.PORT || 3000;

adbf01e71731d2c464264f398e9da74d99caa5bd
Samuel Raducan <samuel.thembp@gmail.com>
2022-04-16 20:55:32 +0100

    Add post method to create data

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const Joi = require('joi');
 const express = require('express');
+const { join } = require('path');
 const app = express();
+
+// Middleware for json data
+app.use(express.json());
 /**
  * http://vidly.com/api/generes
  *
@@ -14,17 +19,28 @@ const app = express();
  */
 
 const genres = [
-	'Action',
-	'Thriller',
-	'Romance',
-	'Drama',
-	'Science Fiction',
-	'Test',
+	{ id: 1, genre: 'Action' },
+	{ id: 2, genre: 'Thriller' },
+	{ id: 3, genre: 'Romance' },
+	{ id: 4, genre: 'Drama' },
+	{ id: 5, genre: 'Science Fiction' },
 ];
 
 // Get request for all the genres
 app.get('/api/genres', (req, res) => {
 	res.send(genres);
+});
+
+app.post('/api/genres', (req, res) => {
+	const genre = {
+		id: genres.length + 1,
+		genre: req.body.genre,
+	};
+
+	genres.push(genre);
+
+	// Send the genre in the body of the response
+	res.send(genre);
 });
 
 // Dynamic PORT

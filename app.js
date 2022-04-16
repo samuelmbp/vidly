3b8d4ab0f8b16c730276c1325f3671fe97608672
Samuel Raducan <samuel.thembp@gmail.com>
2022-04-16 19:51:01 +0100

    Create the get method to retrieve data

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-
+const express = require('express');
+const app = express();
 /**
  * http://vidly.com/api/generes
- * 
- * http://localhost:3000/api/generes
- * 
- * TODO: 
- * Requests: 
+ *
+ * http://localhost:3000/api/genres
+ *
+ * TODO:
+ * Requests:
  *  - get the genres
  *  - post (create a new one)
- *  - update 
- *  - delete 
+ *  - update
+ *  - delete
  */
 
+const genres = [
+	'Action',
+	'Thriller',
+	'Romance',
+	'Drama',
+	'Science Fiction',
+	'Test',
+];
+
+// Get request for all the genres
+app.get('/api/genres', (req, res) => {
+	res.send(genres);
+});
+
+// Dynamic PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Listening on port ${port}...`);
+});

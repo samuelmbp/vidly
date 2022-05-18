da910e4961f89a14db8a2026966764e79ea8bfd6
Samuel Raducan <samuel.thembp@gmail.com>
2022-05-18 17:21:10 +0100

    Handle recject promise with try and catch block in genres routes

--- routes/genres.js
+++ routes/genres.js
@@ -5,8 +5,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-	const genres = await Genre.find().sort('name');
-	res.send(genres);
+	try {
+		const genres = await Genre.find().sort('name');
+		res.send(genres);
+	} catch (error) {
+		res.status(500).send('Something failed.');
+	}
 });
 
 router.get('/:id', async (req, res) => {
